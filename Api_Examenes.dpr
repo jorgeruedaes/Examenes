a4a41d6a4b97f5652ce4f858e563d4b95f8ed4a1
jorgeruedaes@hotmail.com <jorgeruedaes>
2021-02-08 22:08:44 -0500

    Generación de los metodos de inicio de sesión y generaciones de tokens asociados.

--- Api_Examenes.dpr
+++ Api_Examenes.dpr
@@ -10,7 +10,12 @@ uses
   usPrincipal in 'usPrincipal.pas' {fExamenes},
   ServerMethods in 'ServerMethods.pas' {Metodos: TDataModule},
   ServerContainer in 'ServerContainer.pas' {ServerContainer1: TDataModule},
-  WebModule in 'WebModule.pas' {WebModule1: TWebModule};
+  WebModule in 'WebModule.pas' {WebModule1: TWebModule},
+  uSesion in 'Controls\uSesion.pas',
+  uAutorizacion in 'Controls\uAutorizacion.pas',
+  uDBModulo in 'Model\uDBModulo.pas' {DBModulo: TDataModule},
+  uMensajes in 'Controls\uMensajes.pas',
+  UFuncionesGenerales in 'Controls\UFuncionesGenerales.pas';
 
 {$R *.res}
 
@@ -19,5 +24,6 @@ begin
     WebRequestHandler.WebModuleClass := WebModuleClass;
   Application.Initialize;
   Application.CreateForm(TfExamenes, fExamenes);
+  Application.CreateForm(TDBModulo, DBModulo);
   Application.Run;
 end.
--- ServerMethods.pas
+++ ServerMethods.pas
@@ -3,56 +3,23 @@ unit ServerMethods;
 interface
 
 uses System.SysUtils, System.Classes, System.Json,
-  Datasnap.DSServer, Datasnap.DSAuth, Data.DBXFirebird, Data.DB, Data.SqlExpr,
-  Data.FMTBcd, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error,
-  FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool,
-  FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB, FireDAC.Phys.FBDef,
-  FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
-  FireDAC.DApt, FireDAC.Comp.DataSet, FireDAC.Comp.Client, System.IOUtils,
-  IniFiles, FireDAC.Phys.PG, FireDAC.Phys.PGDef, FireDAC.Phys.Oracle,
-  FireDAC.Phys.OracleDef, FireDAC.Phys.MySQLDef, FireDAC.Phys.MySQL,
-  IdGlobal, IdHash, IdHashMessageDigest, System.Hash;
-
-Var
-  Archivo: String;
-  IniFile: TIniFile;
-  UserName: String;
-  Password: String;
-  BaseDato: String;
-  DriverName: String;
-  RutaBaseDato: String;
-  Port: String;
+  Datasnap.DSServer, Datasnap.DSAuth, System.IOUtils, System.Hash;
 
 type
 {$METHODINFO ON}
   TMetodos = class(TDataModule)
-    sqlFBConexion: TFDConnection;
-    sqlQuery: TFDQuery;
-    FDPhysOracleDriverLink1: TFDPhysOracleDriverLink;
-    FDPhysPgDriverLink1: TFDPhysPgDriverLink;
-    FDPhysMySQLDriverLink1: TFDPhysMySQLDriverLink;
-    procedure sqlFBConexionBeforeConnect(Sender: TObject);
-    procedure DataModuleCreate(Sender: TObject);
   private
     { Var privates }
-    FUsuario: String;
 
-    { Private generals declarations }
-    function  Conexion: Boolean;
-    procedure CerrarConexion;
-    procedure ConfirmarConexion;
-    procedure CancelarConexion;
-    procedure EstablecerVariablesConexion;
-    procedure CargarVariablesIniciales;
-    function  GenerarToken(pValue: String): String;
-    function  ValidarToken(pAuthorization: String): Boolean;
-    function  GetTokenUsuario(pAuthorization: String): Boolean;
   public
     { Public example declarations }
     function EchoString(Value: string): string;
     function ReverseString(Value: string): string;
 
-    { Web Services }
+    { }
+    function sIniciarSesion(pAuthorization, pUser, pContrasena: String): String;
+
+    { Examples }
     function Datos(pAuthorization, pValue, pParams: String): TJSONArray;
     function UpdateDatos(pAuthorization, pTable: String;
       LJsonObjet: TJSONObject): Boolean;
@@ -61,7 +28,6 @@ type
       LJsonObjet: TJSONObject): Boolean;
 
     { Web properties }
-    property Usuario: String Read FUsuario Write FUsuario;
 
   end;
 {$METHODINFO OFF}
@@ -70,7 +36,7 @@ implementation
 
 {$R *.dfm}
 
-uses System.StrUtils;
+uses System.StrUtils, uAutorizacion, USesion;
 
 function TMetodos.AcceptDatos(pAuthorization, pTable: String;
   LJsonObjet: TJSONObject): Boolean;
@@ -84,9 +50,9 @@ begin
   CadenaValues := '';
   try
     try
-      if GetTokenUsuario(pAuthorization) then
+      if true then
       begin
-        if Conexion then
+        if true then
         begin
 
           if LJsonObjet.Count > 0 then
@@ -107,129 +73,60 @@ begin
               end;
             end;
 
-            sqlQuery.Close;
-            sqlQuery.SQL.Clear;
-            sqlQuery.SQL.Add('INSERT INTO ' + pTable + '(' + CadenaFields + ')'
+            { sqlQuery.Close;
+              sqlQuery.SQL.Clear;
+              sqlQuery.SQL.Add('INSERT INTO ' + pTable + '(' + CadenaFields + ')'
               + 'values(' + CadenaValues + ')');
-            sqlQuery.ExecSQL;
-            ConfirmarConexion;
+              sqlQuery.ExecSQL;
+              ConfirmarConexion;
 
-            if sqlQuery.RowsAffected > 0 then
-              Result := True;
+              if sqlQuery.RowsAffected > 0 then
+              Result := True; }
+
           end;
         end;
       end;
     except
       on E: Exception do
-        CancelarConexion;
+
     end;
 
   finally
-    CerrarConexion;
 
   end;
 
-end;
-
-procedure TMetodos.CancelarConexion;
-begin
-
-  try
-    sqlFBConexion.Rollback;
-  except
-    on E: Exception do
-      raise Exception.Create(E.Message);
-  end;
 end;
 
 function TMetodos.CancelDatos(pAuthorization, pValue: String): Boolean;
 begin
   Result := False;
   try
-    if GetTokenUsuario(pAuthorization) then
+    if true then
     begin
-      if Conexion then
+      if true then
       begin
 
         try
 
-          sqlQuery.Close;
-          sqlQuery.SQL.Clear;
-          sqlQuery.SQL.Add(pValue);
-          sqlQuery.ExecSQL;
-          ConfirmarConexion;
+          { sqlQuery.Close;
+            sqlQuery.SQL.Clear;
+            sqlQuery.SQL.Add(pValue);
+            sqlQuery.ExecSQL;
+            ConfirmarConexion;
 
-          if sqlQuery.RowsAffected > 0 then
-            Result := True;
+            if sqlQuery.RowsAffected > 0 then
+            Result := True; }
 
         except
           on E: Exception do
-            CancelarConexion;
+
         end;
       end;
     end;
   finally
-    CerrarConexion;
+
   end;
 
-end;
-
-procedure TMetodos.CargarVariablesIniciales;
-begin
-  Archivo := GetCurrentDir + '\' + 'server.ini';
-  IniFile := TIniFile.Create(Archivo);
-  RutaBaseDato := IniFile.ReadString('PARAMETERS', 'PATH', '');
-  BaseDato := IniFile.ReadString('PARAMETERS', 'NAME', '');
-  UserName := IniFile.ReadString('PARAMETERS', 'USER', '');
-  Password := IniFile.ReadString('PARAMETERS', 'PASS', '');
-  DriverName := IniFile.ReadString('PARAMETERS', 'DRIVER', '');
-  Port := IniFile.ReadString('PARAMETERS', 'PORT', '');
-
-  try
-
-  except
-    on E: Exception do
-      raise Exception.Create('No existe un archivo de configuración. ' +
-        E.Message);
-  end;
-
-end;
-
-procedure TMetodos.CerrarConexion;
-begin
-  sqlFBConexion.Close;
-end;
-
-function TMetodos.Conexion: Boolean;
-begin
-  Result := False;
-  try
-    if not sqlFBConexion.Connected then
-      sqlFBConexion.Open;
-
-    Result := True;
-  except
-    on E: Exception do
-      raise Exception.Create
-        ('Ha ocurrido un error durante la conexión en el servidor.');
-  end;
-
-end;
-
-procedure TMetodos.ConfirmarConexion;
-begin
-  try
-    sqlFBConexion.Commit;
-  except
-    on E: Exception do
-      raise Exception.Create
-        ('Ha ocurrido un error al confirmar la transacción.');
-  end;
-end;
-
-procedure TMetodos.DataModuleCreate(Sender: TObject);
-begin
-  CargarVariablesIniciales;
 end;
 
 function TMetodos.Datos(pAuthorization, pValue, pParams: String): TJSONArray;
@@ -239,33 +136,33 @@ var
 begin
 
   try
-    if GetTokenUsuario(pAuthorization) then
+    if true then
     begin
-      if Conexion then
+      if true then
       begin
 
         Result := TJSONArray.Create;
 
         try
-          sqlQuery.Close;
-          sqlQuery.SQL.Clear;
-          sqlQuery.SQL.Add('Select * from ' + pValue + ' where 1=1 ' + pParams);
-          sqlQuery.Open;
+          { sqlQuery.Close;
+            sqlQuery.SQL.Clear;
+            sqlQuery.SQL.Add('Select * from ' + pValue + ' where 1=1 ' + pParams);
+            sqlQuery.Open;
 
-          sqlQuery.First;
-          while not sqlQuery.Eof do
-          begin
+            sqlQuery.First;
+            while not sqlQuery.Eof do
+            begin
             FJsonObject := TJSONObject.Create;
 
             for i := 0 to sqlQuery.FieldCount - 1 do
             begin
-              FJsonObject.AddPair(sqlQuery.Fields[i].FullName,
-                sqlQuery.Fields[i].AsString);
+            FJsonObject.AddPair(sqlQuery.Fields[i].FullName,
+            sqlQuery.Fields[i].AsString);
             end;
 
             Result.AddElement(FJsonObject);
             sqlQuery.Next;
-          end;
+            end; }
 
         except
           on E: Exception do
@@ -277,90 +174,29 @@ begin
     end;
 
   finally
-    CerrarConexion;
+
   end;
 
 end;
 
 function TMetodos.EchoString(Value: string): string;
 begin
-  Result := GenerarToken(Value);
+ Result :=  GenerarToken;
 end;
 
-procedure TMetodos.EstablecerVariablesConexion;
+function TMetodos.sIniciarSesion(pAuthorization, pUser,
+  pContrasena: String): String;
 begin
-  try
-    sqlFBConexion.DriverName := DriverName;
-    sqlFBConexion.Params.Values['Database'] := RutaBaseDato + BaseDato;
-    sqlFBConexion.Params.Values['User_Name'] := UserName;
-    sqlFBConexion.Params.Values['Password'] := Password;
-    sqlFBConexion.Params.Values['Port'] := Port;
-  except
-    on E: Exception do
-      raise Exception.Create
-        ('No ha sido posible realizar la conexión con la base de datos ' +
-        E.Message);
+  if IniciarSesion(pAuthorization, pUser, pContrasena) then
+  begin
+  Result := AsignarContrasena(pUser);
   end;
 
-end;
-
-function TMetodos.GenerarToken(pValue: String): String;
-var
-  Key: String;
-begin
-  try
-    Key := DateTimeToStr(date) + pValue;
-    Result := THashSHA2.GetHMAC(pValue, Key, SHA256);
-  except
-    on E: Exception do
-      raise Exception.Create(E.Message);
-  end;
-
-end;
-
-function TMetodos.GetTokenUsuario(pAuthorization: String): Boolean;
-begin
-  Result := False;
-  try
-    if Conexion then
-    begin
-
-      try
-        sqlQuery.Close;
-        sqlQuery.SQL.Clear;
-        sqlQuery.SQL.Add
-          ('Select codigousuario from usuario where token=:token');
-        sqlQuery.ParamByName('token').AsString := pAuthorization;
-        sqlQuery.Open;
-
-        if not sqlQuery.IsEmpty then
-        begin
-          Result := True;
-          Usuario := sqlQuery.FieldByName('codigousuario').AsString;
-        end;
-      except
-        on E: Exception do
-        begin
-          Result := False;
-          raise Exception.Create
-            ('No hay autorización para realizar la acción.');
-        end;
-      end;
-    end;
-
-  finally
-    CerrarConexion;
-  end;
 end;
 
 function TMetodos.ReverseString(Value: string): string;
 begin
-  Result := System.StrUtils.ReverseString(Value);
-end;
-
-procedure TMetodos.sqlFBConexionBeforeConnect(Sender: TObject);
-begin
-  EstablecerVariablesConexion;
+  Result := EncriptarContrasena(Value);
 end;
 
 function TMetodos.UpdateDatos(pAuthorization, pTable: String;
@@ -375,9 +211,9 @@ begin
   Cadena := '';
   try
     try
-      if GetTokenUsuario(pAuthorization) then
+      if true then
       begin
-        if Conexion then
+        if true then
         begin
 
           if LJsonObjet.Count > 0 then
@@ -399,66 +235,25 @@ begin
 
             end;
 
-            sqlQuery.Close;
-            sqlQuery.SQL.Clear;
-            sqlQuery.SQL.Add('UPDATE ' + pTable + ' SET ' + Cadena + ' WHERE  '
+            { sqlQuery.Close;
+              sqlQuery.SQL.Clear;
+              sqlQuery.SQL.Add('UPDATE ' + pTable + ' SET ' + Cadena + ' WHERE  '
               + Llave + '=' + Valor);
-            sqlQuery.ExecSQL;
-            ConfirmarConexion;
+              sqlQuery.ExecSQL;
+              ConfirmarConexion;
 
-            if sqlQuery.RowsAffected > 0 then
-              Result := True;
+              if sqlQuery.RowsAffected > 0 then
+              Result := True; }
           end;
         end;
       end;
     except
       on E: Exception do
-        CancelarConexion;
+
     end;
 
   finally
-    CerrarConexion;
-  end;
 
-end;
-
-function TMetodos.ValidarToken(pAuthorization: String): Boolean;
-var
-  i: Integer;
-  Id, Token: String;
-  LJsonObjet: TJSONObject;
-begin
-  Result := False;
-  try
-
-    LJsonObjet := TJSONObject.ParseJSONValue(pAuthorization) as TJSONObject;
-
-    if LJsonObjet.Count > 0 then
-    begin
-      for i := 0 to LJsonObjet.Count - 1 do
-      begin
-
-        if LJsonObjet.Pairs[i].JsonString.Value = 'id' then
-          Id := LJsonObjet.Pairs[i].JsonValue.Value
-        else if LJsonObjet.Pairs[i].JsonString.Value = 'token' then
-          Token := LJsonObjet.Pairs[i].JsonValue.Value
-
-      end;
-
-      if GetTokenUsuario(pAuthorization) then
-      begin
-        Result := True;
-      end
-      else
-        raise Exception.Create('No hay autorización para realizar la acción.');
-
-    end
-    else
-      raise Exception.Create('No hay autorización para realizar la acción.');
-
-  except
-    on E: Exception do
-      raise Exception.Create('No hay autorización para realizar la acción.');
   end;
 
 end;
